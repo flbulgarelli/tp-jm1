4d9a29217b65669279ea7ef15ffd470ad00a3ffc
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-06-21 15:14:58 -0300

    IMPORTANTE: no agregar funcionalidad no solicitada
    En particular, veo que contemplaron que el usuario tenga un saldo
    que no debe superar. No veo ningun rastro de esta funcionalidad (salvo en el pseudocódigo
    de su entrega anterior; se me pasó marcarselos allí)

--- Tp Anual Compra Entradas/Implementacion/TP Anual Compra Entradas/src/Persona.groovy
+++ Tp Anual Compra Entradas/Implementacion/TP Anual Compra Entradas/src/Persona.groovy
@@ -1,7 +1,11 @@
 
 class Persona {
 	def tipo 
-	def List entradas 
+	def List entradas
+	//FIXME de dónde sale el saldo? No veo de dónde sale este concepto, no está en el enunciado, ni 
+	//en las pregunta al cliente, ni siquiera en sus CU's. No agreguen funcionalidad
+	//que no surja de los requerimientos, porque es esfuerzo invertido en algo que no aporta valor
+	//al sistema 
 	def saldo 
 	
 	public Persona(tipo,saldo) {
