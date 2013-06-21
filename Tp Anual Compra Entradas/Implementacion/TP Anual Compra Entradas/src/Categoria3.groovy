6a8fe9ce42b30c133d56e41898dc3666d4992abe
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-06-21 15:22:48 -0300

    Agregando comentario sobre intercambiabilidad de categorias

--- Tp Anual Compra Entradas/Implementacion/TP Anual Compra Entradas/src/Categoria3.groovy
+++ Tp Anual Compra Entradas/Implementacion/TP Anual Compra Entradas/src/Categoria3.groovy
@@ -1,3 +1,9 @@
+/*FIXME repito un comentario que les hice en un mail anterior:
+ * Les dejo una pregunta: los objetos de su solucion que modelan las categorias y tipos de usuarios, 
+ * ¿son statefull? Dos objetos "tipoJubilado", ¿podrían ser intercambiados? 
+ * ¿Que consencuancias podría traer eso? ¿Como podrían mejorar su diseño en tal sentido? 
+ * Me gustaría ver un análisis (tipo bonus) sobre esto. 
+ */
 class Categoria3 extends Espectaculo{
 	
 	public Categoria3(fecha, bandas)
