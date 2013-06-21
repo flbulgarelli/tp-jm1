a085c0bed94f13206f95b5692880ca7ceb4a7b79
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-06-21 14:52:56 -0300

    IMPORTANTE: la clase Tipo es innecesaria.

--- Tp Anual Compra Entradas/Implementacion/TP Anual Compra Entradas/src/Tipo.groovy
+++ Tp Anual Compra Entradas/Implementacion/TP Anual Compra Entradas/src/Tipo.groovy
@@ -1,5 +1,8 @@
 //FIXME Tipo? Tipo de qué? Ojo con la expresividad del nombre de la clase: Si no hay ningun tipo de organización de paquetes
 //que me de la pista sobre qué significa "Tipo", entonces hay que darle un mejor nombre a la clase.  
+//FIXME porqué existe esta clase? Tiene sentido instanciar un tipo? Existen subclases que usen alguna parte del comportamiento definido acá?
+//La están usando como una interfaz ( están colocando un método que siempre redefinen, lo cual es incorrecto).
+//En un lenguaje como Groovy, esta clase no es necesaria. 
 class Tipo {
 	
 	double getDescuento(double precio){
